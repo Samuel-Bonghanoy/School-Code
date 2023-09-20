5b3efcf086a0ffbdb54ee28d3fd26f8acc219c2c
sethonne <20103261@usc.edu.ph>
2023-09-21 06:14:35 +0800

    Stack practice

--- CIS 2101/Stacks/stack.c
+++ CIS 2101/Stacks/stack.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+void printBits(unsigned int num){
+    unsigned int size = sizeof(unsigned int);
+    unsigned int maxPow = 1<<(size*8-1);
+    printf("MAX POW : %u\n",maxPow);
+    int i=0,j;
+    for(;i<size;++i){
+        for(j=0;j<8;++j){
+            // print last bit and shift left.
+            printf("%u ",num&maxPow);
+            num = num<<1;
+        }
+    }
+}
+
+int main(void)
+{
+  unsigned int size = sizeof(unsigned int);
+  unsigned int maxPow = 1<<(size*8-1);
+
+  printf("%d %d", size, 1<<31);
+
+  return 0;
+}
