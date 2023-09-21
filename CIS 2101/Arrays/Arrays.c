87d6693def86ac3af21e63cdca9018a76af1bf9c
sethonne <20103261@usc.edu.ph>
2023-09-21 17:05:00 +0800

    Added Arrays Operations

--- CIS 2101/Arrays/Arrays.c
+++ CIS 2101/Arrays/Arrays.c
@@ -4,10 +4,10 @@
 
 void initialize(int arr[], int* count){
   int x;
-  printf("yes");
+  
   for(x = 0; x < *count; x++){
     arr[x] = 0;
-    printf("%d", arr[x]);
+    
   }
 
   *count = 0;
@@ -16,6 +16,7 @@ void initialize(int arr[], int* count){
 void display(int arr[], int count){
   int x; 
   for(x = 0; x < count; x++) printf("%d ", arr[x]);
+  printf("\n");
 }
 
 
@@ -28,18 +29,20 @@ int searchElem(int arr[], int count, int elem){
 
 int insertLast(int arr[], int* count, int elem){
   if(*count != MAX){
-    arr[*count++] = elem; 
+    arr[(*count)++] = elem; 
   }
+
 }
 
 int insertFirst(int arr[], int* count, int elem){
   if(*count != MAX){
     int x;
-    for(x = *count - 1; x > 0; x--){
+    for(x = *count; x > 0; x--){
       arr[x] = arr[x - 1];
     }
 
     arr[0] = elem;
+    (*count)++;
   }
 }
 
@@ -58,10 +61,15 @@ void sortArray(int arr[], int count);
 int main(void) 
 {
   int arr[MAX];
-  int count = 0;
+  int count = MAX;
+
   initialize(arr, &count);
-  display(arr, count);
   insertLast(arr, &count, 5);
+  insertLast(arr, &count, 5);
+  insertLast(arr, &count, 5);
+  insertLast(arr, &count, 5);
+  display(arr, count);
+  insertFirst(arr, &count, 1);
   display(arr, count);
   // printf("start");
 
