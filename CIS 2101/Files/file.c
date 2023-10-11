3e51e972d42665452e3620cea68d9e4301f8ea50
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-12 06:36:18 +0800

    feat: added file exercises

--- CIS 2101/Files/file.c
+++ CIS 2101/Files/file.c
@@ -31,11 +31,24 @@ void displayHeader() {
   printf("%-24s", "=========");
   printf("%-3s", "==");
   printf("%-8s", "======");
-  printf("%5s", "====");
+  printf("%5s", "====\n");
 }
 
 int main()
 {
+  FILE *fp;
+  fp = fopen("students.dat", "rb");
+  Studtype buffer;
+
   displayHeader();
+  while(fread(&buffer, sizeof(Studtype), 1, fp) != 0){
+    printf("%d\t\t", buffer.ID);
+    printf("%s\t\t", buffer.name);
+    // printf("%s\t\t", buffer.course);
+    // printf("%d\t\t", buffer.yrLevel);
+    // printf("\n");
+  }
+
+  fclose(fp);
   return 0;
 }
