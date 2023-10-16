62e78cc111dd1396bce74e748b34c5413beeb8a9
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-16 16:10:29 +0800

    feat: installed MUI

--- CIS 2102/midterms-prac/src/App.jsx
+++ CIS 2102/midterms-prac/src/App.jsx
@@ -1,5 +1,11 @@
+import {
+  AddIcCallOutlined,
+  EditNotifications,
+  FavoriteBorder,
+  NavigationRounded,
+} from "@mui/icons-material";
 import "./App.css";
-import { Button } from "@mui/material";
+import { Button, ButtonGroup, Fab } from "@mui/material";
 // import Signin from "./SignIn";
 
 function App() {
@@ -7,6 +13,29 @@ function App() {
     <>
       <Button variant="contained">suckadick</Button>
       {/* <Signin /> */}
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup>
+      <div>
+        <Fab color="primary" aria-label="add">
+          <AddIcCallOutlined />
+        </Fab>
+        <Fab color="secondary" aria-label="edit">
+          <EditNotifications />
+        </Fab>
+        <Fab variant="extended">
+          <NavigationRounded sx={{ mr: 1 }} />
+          Navigate
+        </Fab>
+        <Fab disabled aria-label="like">
+          <FavoriteBorder />
+        </Fab>
+      </div>
     </>
   );
 }
