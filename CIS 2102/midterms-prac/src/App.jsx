452a7b74a3818b16d96e26c85136915cfa4cfd49
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-16 15:59:39 +0800

    feat: installed MUI

---
+++ CIS 2102/midterms-prac/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@mui/material";
+// import Signin from "./SignIn";
+
+function App() {
+  return (
+    <>
+      <Button variant="contained">suckadick</Button>
+      {/* <Signin /> */}
+    </>
+  );
+}
+
+export default App;
