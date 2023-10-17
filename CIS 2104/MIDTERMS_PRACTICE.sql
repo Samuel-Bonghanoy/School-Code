9e1af6df67a26f9381864325f8aa3e533e81f2cc
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-10-17 19:17:55 +0800

    feat: created instagram database

--- CIS 2104/MIDTERMS_PRACTICE.sql
+++ CIS 2104/MIDTERMS_PRACTICE.sql
@@ -4,7 +4,20 @@ USE instagram;
 
 CREATE TABLE users (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE,
+    username VARCHAR(255) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
-)
+);
     
+INSERT INTO users (username) VALUES 
+('Sethone'),
+('Sethtwo'),
+('Seththree');
+
+CREATE TABLE photos (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
