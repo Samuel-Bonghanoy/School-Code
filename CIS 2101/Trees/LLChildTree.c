bc645ed29242144369527469846b12b9c7cbe259
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-06 23:53:23 +0800

    feat: add tree exercises

---
+++ CIS 2101/Trees/LLChildTree.c
@@ -0,0 +1,82 @@
+#include "_header.h"
+
+// * FUNCTION DEFINITIONS HERE
+void Display(TREE T) {
+   int x;
+   Nodetype trav;
+   for(x = 0; x < MAX; x++){
+        printf("[%d] ->", x);
+        for(trav = T.Header[x]; trav != NULL; trav = trav->link){
+            printf("%d-> ",  trav->key);
+        }
+        printf("\n");
+   } 
+}
+
+void Children(node N, TREE T) {
+    if(N >= 0 && N < MAX) {
+        Nodetype trav;
+        printf("[%d] => ", N);
+        for(trav = T.Header[N]; trav != NULL; trav = trav->link){
+        printf("%d->", trav->key);
+        }
+    }
+    
+}
+
+node Parent(int N, TREE T){
+    int x, retVal = -1;
+    Nodetype trav;
+    for(x = 0; x < MAX; x++) {
+        for(trav = T.Header[x]; trav != NULL && trav->key != N; trav = trav->link){}
+    }
+
+    return trav == NULL ? retVal : x;
+}
+
+node Leftmost_Child(node N, TREE T) {
+    return T.Header[N] == NULL ? -1 : T.Header[N]->key;
+}
+
+node Right_Sibling(node N, TREE T) {
+    Nodetype trav;
+    for(trav = T.Header[N]; trav->link != NULL; trav = trav->link){}
+    return trav->key;
+}
+
+
+label Label(node N, TREE T);
+node Root(TREE T);
+
+void Initialize(TREE *T){
+    int x;
+    for(x = 0; x < MAX; x++) {
+        T->Header[x] = NULL;
+    }
+    T->root = -1;
+}
+
+void MakeNULL(TREE *T) {
+    int x;
+    Nodetype trav, temp;
+    for(x = 0; x < MAX; x++) {
+        for(trav = T->Header[x]; trav != NULL; trav = trav->link) {
+            
+        }
+    }
+}
+
+void Insert(node N, TREE *T, int key) {
+        Nodetype newNode = (Nodetype)malloc(sizeof(Nodetype));
+        newNode->key = key;
+        newNode->link = NULL;
+    if(T->Header[N] == NULL) {
+        T->Header[N] = newNode;
+    } else {
+        Nodetype* trav;
+        for(trav = &(T->Header[N]); trav != NULL; trav = &(*trav)->link) {}
+        newNode->key = key;
+        newNode->link = NULL;
+        *trav = newNode;
+    }
+}
--- CIS 2101/Trees/_header.h
+++ CIS 2101/Trees/_header.h
@@ -1,20 +1,33 @@
 #ifndef DATASTRUCTURE_IMPLEMENTATIONNAME
 #define DATASTRUCTURE_IMPLEMENTATIONNAME
-
+#include <stdio.h>
+#include <stdlib.h>
 // * MACROS HERE
 #define MAX 10
 
 // * TYPE DEFINITIONS HERE
+typedef struct Node {
+    int key;
+    struct Node* link;
+} *Nodetype;
+
 typedef struct {
-    int someIntegerData[MAX];
-    char someCharData[MAX];
-} DataStructureName;
+    Nodetype Header[MAX];
+    int root;
+} TREE;
+
+typedef int node;
+typedef int label;
 
 // * OPERATIONS HERE
-void initializeStruct();
-void addToStruct();
-void deleteFromStruct();
-void updateStruct();
-void displayStruct();
+void Display(TREE T);
+void Children(node N, TREE T);
+node Parent(int N, TREE T);
+node Leftmost_Child(node N, TREE T);
+node Right_Sibling(node N, TREE T);
+label Label(node N, TREE T);
+node Root(TREE T);
+void Initialize(TREE *T);
+void MakeNULL(TREE *T);
 
 #endif
