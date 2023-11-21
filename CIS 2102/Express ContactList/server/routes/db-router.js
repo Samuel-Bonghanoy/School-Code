2567149cb146849889d6cca1a69e19fbc43facbb
Samuel-Bonghanoy <20103261@usc.edu.ph>
2023-11-21 18:09:33 +0800

    feat: add contact list express activity

--- CIS 2102/Express ContactList/server/routes/db-router.js
+++ CIS 2102/Express ContactList/server/routes/db-router.js
@@ -10,14 +10,31 @@ router.get('/createdb', (req, res) => {
 
     res.send(result);
   });
+});
 
+router.get('/createtable', (req, res) => {
   connection.query(
     `CREATE TABLE persons (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     age INT,
     course VARCHAR(100)
 );`,
+    (err, result) => {
+      if (err) console.log(err);
+
+      res.send(result);
+    },
+  );
+});
+
+router.post('/populate', (req, res) => {
+  connection.query(
+    `INSERT INTO persons (id, name, age, course)
+    VALUES
+        (1, 'Samule', 20, 'Computer Science'),
+        (2, 'Mary sucks', 22, 'Mathematics'),
+        (3, 'JDG >>>', 28, 'Physics');`,
     (err, result) => {
       if (err) console.log(err);
 
